506a6c2cca2b335d2da3321f9a58c859360af989
Carlos Alberto Morelos Escalera <169103268+cmorelos523@users.noreply.github.com>
2025-03-09 21:19:35 -0700

    Update latlong.py

--- scripts/latlong.py
+++ scripts/latlong.py
@@ -4,3 +4,8 @@ import json
 
 def main():
     session = gps.gps(mode=gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+    while True:
+        report = session.next()
+        if report['class'] == 'TPV':
+            lat = getattr(report, 'lat', None)
+            lon = getattr(report, 'lon', None)
